c569d4baf85bc7f90a3ae4034804cdd9f68ca9a4
Michael Giebner <mikegiebner@gmail.com>
2019-10-13 07:57:52 -0400

    # Repo: REACT-Section-2 # Repo Description: Colt Steele's Modern React Bootcamp, Section 2, Introducing JSX # Chapter 13: Conditionals in JSX    Added a directory to hold the different chapter files    https://blog.logrocket.com/conditional-rendering-in-react-c6b0e5af381e/

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ function getMood() {
 
 class JSXDemo extends React.Component {
    render() {
+      var msg  = ''
+      var mood = getMood()
+      switch(mood) {
+         case 'angry':
+         msg = 'Angry: I need some space'
+         break
+         case 'hungry':
+         msg = 'Hungry: Lets eat!'
+         break
+         case 'silly':
+         msg = 'Silly: I feel silly'
+         break
+         case 'quite':
+         msg = 'Quiet: Can we not talk right now'
+         break
+         case 'paranoid':
+         msg = 'Paranoid: Is that guy following us?'
+         break
+      }
       return (
          <div> 
-            <h1>My current mood is: {getMood()}</h1>
+            <h1>My current mood is: {msg}</h1>
          </div>
       )
    }
